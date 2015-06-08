168501a86a7b587fc221096c0b3ad7d2029df01c
Anatoly Shipitsin <norguhtar@gmail.com>
2015-06-08 16:41:59 +0500

    Fix filename

--- schema/postgresql/bill.schema.sql
+++ schema/postgresql/bill.schema.sql
@@ -1,3 +1,8 @@
+-- This cake-billing is made available under Open Database License whose 
+-- full text can be found at http://opendatacommons.org/licenses/odbl/. Any rights in 
+-- individual contents of the database are licensed under the Database Contents 
+-- License whose text can be found http://opendatacommons.org/licenses/dbcl/
+
 CREATE SCHEMA bill;
 
 CREATE TABLE bill.ledgertype (
